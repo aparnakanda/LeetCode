8eb8031d9a6cf668131627a4cb011901c09af9fc
Aparna Kanda <119393668+aparnakanda@users.noreply.github.com>
2025-08-11 22:40:08 +0530

    Time: 1 ms (40.01%) | Memory: 41.7 MB (44.05%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words=s.split(" ");
+        String[] res=new String[words.length];
+        for(String w:words){
+            int pos=Character.getNumericValue(w.charAt(w.length()-1))-1;
+            res[pos]=w.substring(0,w.length()-1);
+
+        }
+        return String.join(" ",res);
+    }
+}
